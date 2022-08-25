f4753e8fc79cc1d646ce9e190a47bbd8f98f9f08
Theodora Ko <theodorako@Theos-MacBook-Pro.local>
2022-08-25 17:48:50 -0500

    train, test, etc. updated

--- train_utils.py
+++ train_utils.py
@@ -1,11 +1,13 @@
 import torch
 from IPython.core.interactiveshell import InteractiveShell
+
 InteractiveShell.ast_node_interactivity = "all"
 from IPython.display import clear_output
 import math, random, torch, collections, time, torch.nn.functional as F
-import networkx as nx, matplotlib.pyplot as plt, numpy as np
+import networkx as nx, matplotlib.pyplot as plt, numpy as np, sklearn.decomposition as skD
 from functools import wraps
-mask = collections.namedtuple('mask', ('train', 'test')) #tuple to store train mask, test mask
+
+mask = collections.namedtuple('mask', ('train', 'test'))  # tuple to store train mask, test mask
 
 
 def timethis(func):
@@ -14,17 +16,18 @@ def timethis(func):
         start = time.time()
         result = func(*args, **kwargs)
         end = time.time()
-        print("Time to run function '{}': {:.2f} seconds".format(func.__name__, end-start))
+        print("Time to run function '{}': {:.2f} seconds".format(func.__name__, end - start))
         return result
+
     return wrapper
 
 
 def train_one_epoch(model, criterion, optimizer, x, y, edge_index,
-                    train_mask, task): #x is a dictionary
+                    train_mask, task):  # x is a dictionary
     model.train()
     out = model(x, edge_index)
     loss = criterion(out, y) if train_mask is None else criterion(out[train_mask], y[train_mask])
-    _, predicted = torch.max(out.detach(),1)
+    _, predicted = torch.max(out.detach(), 1)
     optimizer.zero_grad()
     loss.backward()
     optimizer.step()
@@ -34,11 +37,11 @@ def train_one_epoch(model, criterion, optimizer, x, y, edge_index,
         if task == 'classification':
             if train_mask is None:
                 length = len(y)
-                accuracy = (predicted == y).sum().item()/length
+                accuracy = (predicted == y).sum().item() / length
                 misclassified = (predicted != y).numpy()
             else:
                 length = len(y[train_mask])
-                accuracy = (predicted[train_mask] == y[train_mask].detach()).sum().item()/length
+                accuracy = (predicted[train_mask] == y[train_mask].detach()).sum().item() / length
                 misclassified = (predicted[train_mask] != y[train_mask]).numpy()
         else:
             accuracy = loss.item()
@@ -46,10 +49,12 @@ def train_one_epoch(model, criterion, optimizer, x, y, edge_index,
 
     return out, loss.item(), accuracy, misclassified
 
-def train(epochs, model, criterion, optimizer, x, y, edge_index,
-          m = mask(None, None),
-          plotting = False, scatter_size = 30, plotting_freq = 5, dim_reduction = 'pca',
+
+def train(epochs, model, criterion, optimizer, x, y, edge_index, indicate=False,
+          m=mask(None, None),
+          plotting=False, scatter_size=30, plotting_freq=5, dim_reduction='pca',
           task='classification'):
+    if indicate: print(f'Training the model')
     dim_reduction_dict = {'pca': visualize_pca, 'tsne': visualize_tsne}
     train_acc_list = []
     test_acc_list = []
@@ -57,38 +62,43 @@ def train(epochs, model, criterion, optimizer, x, y, edge_index,
     for epoch in range(epochs):
         out, loss, train_acc, misclassified = train_one_epoch(model, criterion, optimizer, x, y, edge_index,
                                                               m.train, task=task)
+        loss_list.append(loss)
+
         model.eval()
         _, _, test_acc, _, predictions = test(model, x, y, edge_index, criterion, m.test, task=task)
-        train_acc_list.append(train_acc)
-        loss_list.append(loss)
+        train_acc_list.append(train_acc)  # each epoch, append accuracy to train_acc_list
         test_acc_list.append(test_acc)
         if plotting:
             if epoch % plotting_freq == 0:
                 clear_output(wait=True)
-                dim_reduction_dict[dim_reduction](out, color=y, size = scatter_size, epoch=epoch, loss = loss)
+                dim_reduction_dict[dim_reduction](out, color=y, size=scatter_size, epoch=epoch, loss=loss)
     if plotting:
         if m == mask(None, None):
             plot_acc(train_acc_list)
         else:
             plot_acc(train_acc_list, test_acc_list)
         plot_loss(loss_list)
-    print("Final test accuracy: {:.2f}".format( test_acc_list[-1]))
+    if indicate:
+        print("Final test accuracy: {:.2f}".format(test_acc_list[-1]))
+        print("output: ", predictions)
     return train_acc_list, test_acc_list, loss_list, misclassified, predictions
+    # returns the last predictions
 
-def test(model,  x, y, edge_index, criterion, test_mask, task):
+
+def test(model, x, y, edge_index, criterion, test_mask, task):
     model.eval()
     out = model(x, edge_index)
     loss = criterion(out, y) if test_mask is None else criterion(out[test_mask], y[test_mask])
     if task == 'classification':
-        _, predicted = torch.max(out.detach(),1)
+        _, predicted = torch.max(out.detach(), 1)
         with torch.no_grad():
             if test_mask is None:
                 length = len(y)
-                accuracy = (predicted == y).sum().item()/length
+                accuracy = (predicted == y).sum().item() / length
                 misclassified = (predicted != y).numpy()
             else:
                 length = len(y[test_mask])
-                accuracy = (predicted[test_mask] == y[test_mask].detach()).sum().item()/length
+                accuracy = (predicted[test_mask] == y[test_mask].detach()).sum().item() / length
                 misclassified = (predicted[test_mask] != y[test_mask]).numpy
     else:
         predicted = out
@@ -97,11 +107,10 @@ def test(model,  x, y, edge_index, criterion, test_mask, task):
     return out, loss.item(), accuracy, misclassified, predicted
 
 
-
 def visualize(h, color):
     z = TSNE(n_components=2).fit_transform(out.detach().cpu().numpy())
 
-    plt.figure(figsize=(10,10))
+    plt.figure(figsize=(10, 10))
     plt.xticks([])
     plt.yticks([])
 
@@ -109,7 +118,7 @@ def visualize(h, color):
     plt.show()
 
 
-def plot_acc(train_acc, test_acc=None, xaxis = 'epochs', yaxis = 'accuracy', title = 'Accuracy plot'):
+def plot_acc(train_acc, test_acc=None, xaxis='epochs', yaxis='accuracy', title='Accuracy plot'):
     plt.xlabel(xaxis)
     plt.ylabel(yaxis)
     if test_acc is not None:
@@ -121,10 +130,10 @@ def plot_acc(train_acc, test_acc=None, xaxis = 'epochs', yaxis = 'accuracy', tit
         plt.legend(['train accuracy'], loc='upper right')
     plt.title(title)
     plt.tight_layout()
-    plt.show() #show train_acc and test_acc together
+    plt.show()  # show train_acc and test_acc together
 
 
-def plot_loss(loss, xaxis = 'epochs', yaxis = 'loss', title = 'Loss plot'):
+def plot_loss(loss, xaxis='epochs', yaxis='loss', title='Loss plot'):
     plt.xlabel(xaxis)
     plt.ylabel(yaxis)
     plt.plot(np.arange(len(loss)), loss, color='black')
@@ -132,18 +141,20 @@ def plot_loss(loss, xaxis = 'epochs', yaxis = 'loss', title = 'Loss plot'):
     plt.tight_layout()
     plt.show()
 
+
 def visualize_graph(G, color, size=300, epoch=None, loss=None):
-    plt.figure(figsize=(7,7))
+    plt.figure(figsize=(7, 7))
     plt.xticks([])
     plt.yticks([])
     nx.draw_networkx(G, pos=nx.spring_layout(G, seed=42), with_labels=False,
-                         node_color=color, cmap="Set2", node_size = size)
+                     node_color=color, cmap="Set2", node_size=size)
     plt.show()
 
-def visualize_tsne(out, color, size=30, epoch=None, loss = None):
+
+def visualize_tsne(out, color, size=30, epoch=None, loss=None):
     z = TSNE(n_components=2).fit_transform(out.detach().cpu().numpy())
 
-    plt.figure(figsize=(7,7))
+    plt.figure(figsize=(7, 7))
     plt.xticks([])
     plt.yticks([])
 
@@ -153,11 +164,11 @@ def visualize_tsne(out, color, size=30, epoch=None, loss = None):
     plt.show()
 
 
-def visualize_umap(out, color, size=30, epoch=None, loss = None):
+def visualize_umap(out, color, size=30, epoch=None, loss=None):
     umap_2d = UMAP(n_components=2, init='random', random_state=0)
     z = umap_2d.fit_transform(out.detach().cpu().numpy())
 
-    plt.figure(figsize=(7,7))
+    plt.figure(figsize=(7, 7))
     plt.xticks([])
     plt.yticks([])
     plt.scatter(z[:, 0], z[:, 1], s=size, c=color, cmap="Set2")
@@ -165,9 +176,10 @@ def visualize_umap(out, color, size=30, epoch=None, loss = None):
         plt.xlabel(f'Epoch: {epoch}, Loss: {loss:.4f}', fontsize=16)
     plt.show()
 
+
 def visualize_pca(out, color, size=30, epoch=None, loss=None):
-    h = PCA(n_components=2).fit_transform(out.detach().cpu().numpy())
-    plt.figure(figsize=(7,7))
+    h = skD.PCA(n_components=2).fit_transform(out.detach().cpu().numpy())
+    plt.figure(figsize=(7, 7))
     plt.xticks([])
     plt.yticks([])
 
@@ -176,35 +188,41 @@ def visualize_pca(out, color, size=30, epoch=None, loss=None):
         plt.xlabel(f'Epoch: {epoch}, Loss: {loss:.4f}', fontsize=16)
     plt.show()
 
+
 def delete_undirected_edges(edge_index, p):
     edges = torch.clone(edge_index).t().numpy()
     edges = set(map(frozenset, edges))
     n = len(edges)
-    edges = random.sample(edges, round(n*(1-p)))
+    edges = random.sample(edges, round(n * (1 - p)))
     edges = list(map(list, edges))
-    reverse_edges = list(map(lambda x: [x[1],x[0]], edges))
+    reverse_edges = list(map(lambda x: [x[1], x[0]], edges))
     sample = sorted(edges + reverse_edges)
     return torch.tensor(sample).t().contiguous()
 
-def add_undirected_edges(edge_index, edge_num, node_total = 2708):
+
+def add_undirected_edges(edge_index, edge_num, node_total=2708):
     edges = torch.clone(edge_index).t().numpy()
     edges = set(map(frozenset, edges))
     n = len(edges)
     while len(edges) - n < edge_num:
         edges.add(frozenset(random.sample(range(node_total), 2)))
     edges = list(map(list, edges))
-    reverse_edges = list(map(lambda x: [x[1],x[0]], edges))
+    reverse_edges = list(map(lambda x: [x[1], x[0]], edges))
     sample = sorted(edges + reverse_edges)
     return torch.tensor(sample).t().contiguous()
 
+
 def dataset_print(dataset):
+    print()
     print(f'Dataset: {dataset}:')
     print('======================')
     print(f'Number of graphs: {len(dataset)}')
     print(f'Number of features: {dataset.num_features}')
     print(f'Number of classes: {dataset.num_classes}')
 
+
 def data_print(data):
+    print()
     print(data)
     print('==============================================================')
 
@@ -217,3 +235,12 @@ def data_print(data):
     print(f'Contains isolated nodes: {data.contains_isolated_nodes()}')
     print(f'Contains self-loops: {data.contains_self_loops()}')
     print(f'Is undirected: {data.is_undirected()}')
+
+
+def loo_print(new_output, y_differences, kl, improved_accuracy):
+    for node_idx in output.keys():
+        print(f'masking {node_idx} improves the accuracy to {improved_accuracy[node_idx]}')
+        print(f' - kl score {kl[node_idx]}')
+        print(f' - output difference: {y_differences[node_idx]}')
+
+
